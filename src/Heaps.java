13641912b369c8f63960dbd65418b57d263984a6
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-04-15 23:15:58 -0500

    creating file

---
+++ src/Heaps.java
@@ -0,0 +1,28 @@
+public class Heaps {
+    public heap_ini(keys,n){
+
+    }
+
+    in_heap(id){
+
+    }
+
+    min_key(){
+
+    }
+
+    min_id(){
+
+    }
+
+    key(id){
+
+    }
+
+    delete_min(){
+
+    }
+    decrease_key(){
+
+    }
+}
